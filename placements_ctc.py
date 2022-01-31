13e3e4bf0545c06803810a8bb5088dc7b9c6f7de
gvevance <gvevance@gmail.com>
2022-01-31 17:08:32 +0530

    Login complete

--- placements_ctc.py
+++ placements_ctc.py
@@ -2,6 +2,7 @@
 
 # Reference 1 - https://www.youtube.com/watch?v=RsQ1tFLwldY&t=306s
 # Reference 2 - https://docs.python-requests.org/en/latest/user/quickstart/
+# Reference 3 - https://www.youtube.com/watch?v=fmf_y8zpOgA
 
 # Step 1 - Figure out login page. Login into portal.
 # Step 2 - click companies hyperlink and go to the next page
@@ -15,12 +16,20 @@ login_page = 'https://placement.iitm.ac.in/students/login.php'
 
 def main():
     
-    source = requests.get(login_page).text
-    soup = BeautifulSoup(source,'lxml')
-    # print(soup.prettify())
+    payload = {
+        'rollno' : 'ee17b105' ,
+        'pass' : 'pen-paper-movie@20-21' ,
+        'submit' : 'Login'
+    }
+    
+    with requests.Session() as s :
+        
+        source = s.post(login_page,data=payload).text
+        soup = BeautifulSoup(source,'lxml')    
+        for tag in soup.find_all("a"):
+            print(tag.text)
 
-    match = soup.find("title")
-    print(match)
 
 if __name__ == "__main__" :
     main()
+
