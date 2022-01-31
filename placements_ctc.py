fc693fa4dd276033a95cba626b4f79d12e2775c0
gvevance <gvevance@gmail.com>
2022-01-31 18:14:02 +0530

    checkpoint A

--- placements_ctc.py
+++ placements_ctc.py
@@ -9,7 +9,7 @@
 # Step 3 - Loop through relevant job profiles. Click on each profile and retrieve details.
 # Step 4 - Store it in a database or something similar (learn databases)
 
-from turtle import onclick
+import numpy as np
 import requests
 from bs4 import BeautifulSoup
 
@@ -19,24 +19,34 @@ def main():
     
     payload = {
         'rollno' : 'ee17b105' ,
-        'pass' : 'pen-paper-movie@20-21' ,
+        'pass'   : 'pen-paper-movie@20-21' ,
         'submit' : 'Login'
     }
     
     with requests.Session() as s :
         
+        # Print all links
         source = s.post(login_page,data=payload).text
         soup = BeautifulSoup(source,'lxml')    
-        for tag in soup.find_all("a"):
-            print(tag.text)
+        for item in soup.find_all("a"):
+            print(item.text)
 
         url_all_companies = 'https://placement.iitm.ac.in/students/comp_list_all.php'
         source = s.get(url_all_companies).text
         soup = BeautifulSoup(source,'lxml')
-        for tag in soup.find_all("a",onclick='OpenPopup(this.href); return false'):
-            print(tag.text)
+        str1 = 'https://placement.iitm.ac.in/students/'
+        links = []
+        
+        for item in soup.find_all("a",onclick='OpenPopup(this.href); return false'):
+            links.append(str1+item['href'])
 
+        source = s.get(links[53]).text
+        soup = BeautifulSoup(source,'lxml')
+        for item in soup.find_all("td",width="13%"):
+            print(item.text)
+
+        
+
+        
 if __name__ == "__main__" :
     main()
-
-# <a href="view_profile.php?cid=250&amp;pid=958" onclick="OpenPopup(this.href); return false" style="text-decoration:underline"><font color="blue">Safety Software Engineer</font></a>
