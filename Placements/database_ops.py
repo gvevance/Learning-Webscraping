d90dc0113fc50c956ca10ffa2b953614d9f5d2a2
gvevance <gvevance@gmail.com>
2022-02-08 21:45:13 +0530

    Database filling

--- Placements/database_ops.py
+++ Placements/database_ops.py
@@ -23,6 +23,71 @@ def db_init(database):
     return conn,c
 
 
+def insert_data(table_name,title,designation,offer_nature,pay_details,conn,c):
+    
+    currency = pay_details[0]
+    ctc = int(pay_details[1])
+    gross_taxable = int(pay_details[2])
+    fixed_basic_pay = int(pay_details[3])
+    others = pay_details[4]
+
+    text = f'''INSERT INTO {table_name} VALUES (?,?,?,?,?,?,?,?)'''
+    # dict = {"Title"                 : title ,
+    #         "Designation"           : designation ,
+    #         "Nature"                : offer_nature ,
+    #         "Currency"              : currency ,
+    #         "CTC"                   : ctc ,
+    #         "Gross"                 : gross_taxable ,
+    #         "Fixed"                 : fixed_basic_pay ,
+    #         "Others"                : others }
+    
+    tuple_ = (title,designation,offer_nature,currency,ctc,gross_taxable,fixed_basic_pay,others)
+
+    try :
+        c.execute(text,tuple_)
+    except Exception as e :
+        print("New error type found 3.")
+        print(e)
+        input()
+
+        
+def update_database(title,designation,offer_nature,payslabs,conn,c):
+    
+    print(title,designation,offer_nature)
+    for key in payslabs :
+        try :
+            for branch in payslabs[key][5]:
+                text = f""" CREATE TABLE "{key} {branch}"(
+                        Title text ,
+                        Designation text ,
+                        "Nature of Offer" text ,
+                        Currency text ,
+                        CTC integer ,
+                        "Gross Taxable Income" integer ,
+                        "Fixed Basic Pay" integer ,
+                        Others text
+                        )"""
+                try :
+                    c.execute(text)
+                
+                except sqlite3.OperationalError :
+                    print(f"Table already exists error.")
+                
+                except Exception as e :
+                    print("New error type found 1")
+                    print(e)
+                    input()
+
+                table_name = f'''"{key} {branch}"'''
+                insert_data(table_name,title,designation,offer_nature,payslabs[key],conn,c)
+
+        except IndexError :
+            print("Bad data. Check it out.")
+        except Exception as e:
+            print("New error type found 2")
+            print(e)
+            input()
+
 def repopulate_db(exists):
 
     if exists :
@@ -60,30 +125,7 @@ def repopulate_db(exists):
             update_database(title,designation,offer_nature,payslabs,conn,c)
         
         print(f"Bad data count = {bad_count}")
+        conn.commit()
+        conn.close()
 
 
-def update_database(title,designation,offer_nature,payslabs,conn,c):
-    
-    print(title,designation,offer_nature)
-    for key in payslabs :
-        try :
-            for branch in payslabs[key][5]:
-                text = f""" CREATE TABLE "{key} {branch}"(
-                        Title text ,
-                        Designation text ,
-                        "Nature of Offer" text ,
-                        CTC integer ,
-                        "Gross Taxable Income" integer ,
-                        "Fixed Basic Pay" integer ,
-                        Others text
-                        )"""
-                
-            c.execute(text)
-        except IndexError :
-            print("Bad data. Check it out.")
-        except sqlite3.OperationalError:
-            print(f"Table already exists error.")
-        except :
-            input("New error type found.")
-
-        
