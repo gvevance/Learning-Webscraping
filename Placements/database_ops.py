b056fbf63edc16a68116e3a856e8818595759c7f
gvevance <gvevance@gmail.com>
2022-02-09 15:38:08 +0530

    version 1 complete

--- Placements/database_ops.py
+++ Placements/database_ops.py
@@ -28,44 +28,33 @@ def insert_data_db(profile,table_set,c):
     
     for key in profile.get_payslabs_keys():
 
-        currency = profile.get_currency(key)
-        ctc = profile.get_ctc(key)
-        gross_taxable = profile.get_gross(key)
-        fixed_pay = profile.get_fixed_pay(key)
-        others = profile.get_others(key)
+        if profile.check_payslabs_health(key) == "OK" :
+            
+            currency = profile.get_currency(key)
+            ctc = profile.get_ctc(key)
+            gross_taxable = profile.get_gross(key)
+            fixed_pay = profile.get_fixed_pay(key)
+            others = profile.get_others(key)
 
-        for branch in profile.get_branch_list(key):
+            for branch in profile.get_branch_list(key):
 
-            if branch == "All" :
-                pass
-                # for i in get_relevant_branches(key,table_set) :
-                #     pass
+                if branch == "All" :
+                    pass
+                    # for i in get_relevant_branches(key,table_set) :
+                    #     pass
 
-            else :
+                else :
 
-                table_name = f"{key} {branch}"
-                text = f'''INSERT INTO "{table_name}" VALUES (?,?,?,?,?,?,?,?)''' 
-                info_tuple_ = (title,designation,offer_nature,currency,ctc,gross_taxable,fixed_pay,others)
-                
-                try :
-                    c.execute(text,info_tuple_)
+                    table_name = f"{key} {branch}"
+                    text = f'''INSERT INTO "{table_name}" VALUES (?,?,?,?,?,?,?,?)''' 
+                    info_tuple_ = (title,designation,offer_nature,currency,ctc,gross_taxable,fixed_pay,others)
+                    
+                    try :
+                        c.execute(text,info_tuple_)
 
-                except Exception as e:
-                    print(e)
-                    input()
-
-
-
-    # text = f'''INSERT INTO "{table_name}" VALUES (?,?,?,?,?,?,?,?)''' 
-    # tuple_ = (title,designation,offer_nature,currency,ctc,gross_taxable,fixed_basic_pay,others)
-
-    # try :
-    #     c.execute(text,tuple_)
-    #     # print(f"Entry added to {table_name}")
-    # except Exception as e :
-    #     print("New error type found 3.")
-    #     print(e)
-    #     input()
+                    except Exception as e:
+                        print(e)
+                        input()
 
 
 def create_table_in_db(table_name,c) :
@@ -136,13 +125,11 @@ def update_database(table_set,c):
         try :
             
             profile = pickle.load(pfile)
+            
             if profile.check_health() == "OK" :
                 
-                for key in profile.get_payslabs_keys():
-                    
-                    if profile.check_payslabs_health(key) == "OK" :
+                insert_data_db(profile,table_set,c)  
 
-                        insert_data_db(profile,table_set,c)            
             else :
                 print(profile.check_health())
 
@@ -151,30 +138,6 @@ def update_database(table_set,c):
     
     pfile.close()
 
-    
-    # for key in payslabs :
-        
-    #     try :
-            
-    #         for branch in payslabs[key][5] :
-                
-    #             if branch == "All" :
-    #                 # get all tables which start with key and insert to each one
-    #                 pass
-
-    #             else :
-    #                 table_name = f"{key} {branch}"
-    #                 insert_data(table_name,title,designation,offer_nature,payslabs[key],c)
-
-    #     except IndexError :
-    #         # print("Bad data. Check it out.")
-    #         print("Bad data")
-
-    #     except Exception as e:
-    #         print("Error found at table update except clause.")
-    #         print(e)
-    #         input()
-
 
 def populate_db(file_exists):
 
@@ -182,7 +145,7 @@ def populate_db(file_exists):
     pickle_file_populate()
 
     if file_exists :
-        with open(database,'w') :       # clear file     
+        with open(database,'w') :       # clear database file     
             pass
 
     conn,c = db_init(database)
